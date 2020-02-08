138f87a108aa970620860465bc6b2e2c4b951179
Johnny Navarro <johnnyjamesnavarro@gmail.com>
2020-02-08 18:47:32 -0500

    Added Game Screen and Game Over Screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -16,7 +16,7 @@ import Input from "../components/Input";
 import Colors from "../constants/colors";
 import NumberContainer from "../components/NumberContainer";
 
-export default function StartGameScreen() {
+export default function StartGameScreen(props) {
   const [enteredValue, setEnteredValue] = useState("");
   const [confirmed, setConfirmed] = useState(false);
   const [selectedNumber, setSelectedNumber] = useState();
@@ -55,7 +55,10 @@ export default function StartGameScreen() {
       <Card style={styles.summaryContainer}>
         <Text style={styles.selectedTitle}>You Selected</Text>
         <NumberContainer>{selectedNumber}</NumberContainer>
-        <Button title="START GAME" />
+        <Button
+          title="START GAME"
+          onPress={() => props.onStartGame(selectedNumber)}
+        />
       </Card>
     );
   }
